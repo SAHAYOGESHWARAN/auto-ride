29eeb0f0634fc8b5b4ef14f4eabd17aca2b74459
sahayogeshwaranp <155245416+SAHAYOGESHWARAN@users.noreply.github.com>
2025-01-20 11:15:15 +0530

    testing

--- backend/controllers/autoDriverController.js
+++ backend/controllers/autoDriverController.js
@@ -5,3 +5,18 @@ const router = express.Router();
 router.post('/register', registerDriver);
 
 module.exports = router;
+
+const AutoDriver = require('../models/AutoDriver');
+
+const registerDriver = async (req, res) => {
+    const { name, vehicle, licensePlate } = req.body;
+    try {
+        const newDriver = new AutoDriver({ name, vehicle, licensePlate });
+        await newDriver.save();
+        res.status(201).json(newDriver);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+module.exports = { registerDriver };
